a05b32dfcdaf574aff770e2c15bbfd18a5959450
Evgeny Zhulenev <evgeny.zhulenev@gmail.com>
2012-07-19 23:26:41 +0400

    TEst fix

--- cgate/src/main/scala/com/ergodicity/cgate/config/CGateConfig.scala
+++ cgate/src/main/scala/com/ergodicity/cgate/config/CGateConfig.scala
@@ -3,7 +3,7 @@ package com.ergodicity.cgate.config
 import java.io.File
 
 case class CGateConfig(ini: File, key: String) {
-  if (!ini.exists()) throw new IllegalArgumentException("CGate ini file doesn't exists")
+  if (!ini.exists()) throw new IllegalArgumentException("CGate ini file doesn't exists: " + ini.getAbsolutePath)
 
   val config = "ini=" + ini.getAbsolutePath + ";key=" + key
 
