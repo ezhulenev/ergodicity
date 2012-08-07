e8b99326d0a01e0a6cc50e07a22a5b9b34da8ce6
Evgeny Zhulenev <evgeny.zhulenev@gmail.com>
2012-08-07 13:00:20 +0400

    Broker integration test

--- core/src/it/scala/integration/ergodicity/core/BrokerIntegrationSpec.scala
+++ core/src/it/scala/integration/ergodicity/core/BrokerIntegrationSpec.scala
@@ -3,7 +3,7 @@ package integration.ergodicity.core
 import java.io.File
 import akka.actor.{Props, Actor, ActorSystem}
 import AkkaIntegrationConfigurations._
-import akka.testkit.{ImplicitSender, TestFSMRef, TestKit}
+import akka.testkit.{ImplicitSender, TestKit}
 import java.util.concurrent.TimeUnit
 import ru.micexrts.cgate.{Connection => CGConnection, Listener => CGListener, Publisher => CGPublisher, P2TypeParser, CGate}
 import com.ergodicity.cgate.{Error => _, _}
@@ -130,5 +130,63 @@ class BrokerIntegrationSpec extends TestKit(ActorSystem("BrokerIntegrationSpec",
 
     Thread.sleep(TimeUnit.DAYS.toMillis(10))
 
+  }
+
+  "order and cancel them later" in {
+
+    val underlyingConnection = new CGConnection(RouterConnection())
+    val connection = system.actorOf(Props(new Connection(underlyingConnection, Some(500.millis))), "Connection")
+
+    val messagesConfig = FortsMessages(BrokerName, 5.seconds, new File("./cgate/scheme/forts_messages.ini"))
+    val underlyingPublisher = new CGPublisher(underlyingConnection, messagesConfig())
+
+    val broker = system.actorOf(Props(new Broker(BindPublisher(underlyingPublisher) to connection)), "Broker")
+
+    val underlyingListener = new CGListener(underlyingConnection, Replies(BrokerName)(), new ReplySubscriber(broker))
+    val replyListener = system.actorOf(Props(new Listener(BindListener(underlyingListener) to connection)), "ReplyListener")
+
+    // On connection Activated open listeners etc
+    connection ! SubscribeTransitionCallBack(system.actorOf(Props(new Actor {
+      protected def receive = {
+        case Transition(_, _, Active) =>
+          // Open Listener &  Broker
+          broker ! Broker.Open
+          Thread.sleep(2000)
+          replyListener ! Listener.Open(RepliesParams)
+
+          // Process messages
+          connection ! StartMessageProcessing(100.millis)
+      }
+    })))
+
+    // Open connections and track it's status
+    connection ! Connection.Open
+
+    Thread.sleep(3000)
+
+    val f = (broker ? Buy[Futures](Isin("RTS-9.12"), 1, 142000, GoodTillCancelled)).mapTo[Either[ActionFailed, Order]]
+
+    val response = Await.result(f, 5.seconds)
+
+    log.info("Order Response = " + response)
+
+    assert(response match {
+      case err@Right(Order(_)) => true
+      case _ => false
+    })
+
+    response.right.map(order => {
+      val f = (broker ? Cancel[Futures](order)).mapTo[Either[ActionFailed, Cancelled]]
+      val response = Await.result(f, 5.seconds)
+
+      log.info("Cancel Response = " + response)
+
+      assert(response match {
+        case err@Right(Cancelled(_)) => true
+        case _ => false
+      })
+    })
+
+    Thread.sleep(TimeUnit.DAYS.toMillis(10))
   }
 }
